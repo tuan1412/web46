6d15a2f0bfb005bcfcb73f04ddaaa7ffb75cb748
tuan1412 <tuannguyenanh1412@gmail.com>
2021-01-16 22:31:35 +0700

    add node js course

---
+++ nodejs_practice/index.js
@@ -0,0 +1,26 @@
+// Sử dụng các hàm đọc, ghi file đồng bộ để hoàn thiện các yêu cầu sau
+// lấy thông tin học sinh có _id là jubuq3lfmjjmp0wrdeupt
+function getDetailStudent() {
+  return {};
+}
+
+// Lấy số lượng học sinh có từ Nguyễn
+function getCountStudentWithLastName() {
+  return 0;
+}
+
+// Tính điểm trung bình của toàn bộ sinh viên (làm tròn đến một chữ số sau dấu phẩy)
+function calAverageMark() {
+  return 0;
+}
+
+// Ghi ra số lượng học sinh đạt điểm 10 ra file output.txt (sử dụng hàm ghi đồng bộ);
+function writeCountStudentGet10MarkToFile() {
+}
+
+module.exports = {
+  getDetailStudent,
+  getCountStudentWithLastName,
+  calAverageMark,
+  writeCountStudentGet10MarkToFile
+}
