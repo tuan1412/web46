c9560449bdaaf49e87d8fce08eb50c9623ab1e85
tuan1412 <tuannguyenanh1412@gmail.com>
2021-01-18 19:48:13 +0700

    chua bai tap ve nha

--- nodejs_practice/index.js
+++ nodejs_practice/index.js
@@ -1,21 +1,31 @@
 // Sử dụng các hàm đọc, ghi file đồng bộ để hoàn thiện các yêu cầu sau
 // lấy thông tin học sinh có _id là jubuq3lfmjjmp0wrdeupt
+const fs = require('fs');
+// const data = JSON.parse(
+//   fs.readFileSync('./data.json')
+// );
+
+const data = require('./data.json');
+
 function getDetailStudent() {
-  return {};
+  return data.find(student => student._id === 'jubuq3lfmjjmp0wrdeupt');
 }
 
 // Lấy số lượng học sinh có từ Nguyễn
 function getCountStudentWithLastName() {
-  return 0;
+  return data.filter(student => student.name.includes('Nguyễn')).length;
 }
 
 // Tính điểm trung bình của toàn bộ sinh viên (làm tròn đến một chữ số sau dấu phẩy)
 function calAverageMark() {
-  return 0;
+  const sum = data.reduce((acc, cur) => acc + cur.mark, 0);
+  return parseFloat((sum / data.length).toFixed(1));
 }
 
 // Ghi ra số lượng học sinh đạt điểm 10 ra file output.txt (sử dụng hàm ghi đồng bộ);
 function writeCountStudentGet10MarkToFile() {
+  const count = data.filter(student => student.mark === 10).length;
+  fs.writeFileSync('output.txt', `${count}`);
 }
 
 module.exports = {
